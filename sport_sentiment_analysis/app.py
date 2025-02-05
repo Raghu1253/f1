2f51e18de474382d9a0c8a57b37a673ea2baa2da
Raghu1253 <138141019+Raghu1253@users.noreply.github.com>
2025-02-05 10:08:24 +0530

    Update app.py

--- sport_sentiment_analysis/app.py
+++ sport_sentiment_analysis/app.py
@@ -12,7 +12,8 @@ from sklearn.preprocessing import LabelEncoder
 import os
 
 # Paths
-MODEL_PATH = 'C:/Users/Sikkandhar Jabbar/Desktop/sport_sentiment_analysis/sports_sentiment_lstm_model.h5'
+# MODEL_PATH = 'C:/Users/Sikkandhar Jabbar/Desktop/sport_sentiment_analysis/sports_sentiment_lstm_model.h5'
+MODEL_PATH = 'sport_sentiment_analysis/sports_sentiment_lstm_model.h5'
 DEFAULT_TEST_DATA_PATH = 'C:/Users/Sikkandhar Jabbar/Desktop/sport_sentiment_analysis/input/fifa_test_dataset.csv'
 
 # Load Model
